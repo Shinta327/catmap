0a28e991b1682735a49c0f5830d0be40e6508013
KanayamaShinta <shinta327tuba@gmail.com>
2021-08-25 18:07:55 +0000

    layout

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,19 +9,14 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-
 // bootstrapとfontawesomeのための編集
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
-
-
-
 //= require rails-ujs
 //= require activestorage
+//= require_tree .
 
 // 一旦ターボリンクを切る
 // require turbolinks
-
-//= require_tree .
 
